0c6ff368d855553b7fdd00eda9599ccf584ec690
Miguel Lopez <miguel.lopez@nagarro.com>
2023-01-31 10:54:08 -0600

    Initial commit

---
+++ src/main/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.mikauran.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroevoServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroevoServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikauran.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroevoServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
