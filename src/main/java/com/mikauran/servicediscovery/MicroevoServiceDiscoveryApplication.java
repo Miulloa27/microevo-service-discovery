7396e832571bdc3a2462d31ac87dc8dbf20629f6
Miguel Lopez <miguel.lopez@nagarro.com>
2023-01-31 10:55:01 -0600

    Initial setup for Eureka Server
    Service discovery service enable and running

--- src/main/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplication.java
+++ src/main/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.mikauran.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroevoServiceDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroevoServiceDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroevoServiceDiscoveryApplication.class, args);
+    }
 
 }
--- src/test/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplicationTests.java
+++ src/test/java/com/mikauran/servicediscovery/MicroevoServiceDiscoveryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MicroevoServiceDiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
